7e5a0abfbd944f6417d8d7ae8155da2e29c82df1
Kelwin Karan <kelwinkaran@gmail.com>
2025-11-16 17:10:02 -0300

    Add SceneController for level management and update build settings to include Menu scene

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void NextLevel()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void loadScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+}
