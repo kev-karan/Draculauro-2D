c19cad3c4cc8e7a8795326b4a28f8fea3903f6c8
Kelwin Karan <kelwinkaran@gmail.com>
2025-11-16 17:27:27 -0300

    Refactor FinishPoint script to implement level transition logic based on goNextLevel flag

--- Assets/Scripts/FinishPoint.cs
+++ Assets/Scripts/FinishPoint.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class FinishPoint : MonoBehaviour
 {
+    [SerializeField] bool goNextLevel;
+    [SerializeField] string levelName;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneController.Instance.NextLevel();
+            if (goNextLevel)
+            {
+                SceneController.Instance.NextLevel();
+            }
+            else
+            {
+                SceneController.Instance.loadScene(levelName);
+            }
         }
     }
 }
