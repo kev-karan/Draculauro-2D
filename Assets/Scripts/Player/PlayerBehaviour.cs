6a1ae19ee8d21cdd33f31c2a6b6c8d28a8bd5edc
Kelwin Karan <kelwinkaran@gmail.com>
2025-11-18 21:00:24 -0300

    implementado Coyote Time

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -4,10 +4,13 @@ public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 3f;
+    [SerializeField] private float coyoteTime = 0.2f;
 
     private Rigidbody2D rb;
     private IsGroundedChecker isGroundedChecker;
     private InputManager input;
+
+    private float coyoteTimeCounter;
 
     private void Awake()
     {
@@ -23,14 +26,26 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void Update()
     {
+        if (isGroundedChecker.IsGrounded())
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
         float moveDirection = input.Movement;
-        Vector2 vectorMoveDirection = new Vector2(moveDirection * moveSpeed, rb.linearVelocity.y);
-        rb.linearVelocity = vectorMoveDirection;
+        Vector2 move = new Vector2(moveDirection * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = move;
     }
 
     private void HandleJump()
     {
-        if (!isGroundedChecker.IsGrounded()) return;
-        rb.linearVelocity += Vector2.up * jumpForce;
+        if (coyoteTimeCounter <= 0f) return;
+
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
+        coyoteTimeCounter = 0f;
     }
 }
